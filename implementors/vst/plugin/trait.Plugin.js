1021062e7fa074e89efe7537af81ee6cf8fe557f
Travis CI User <travis@example.org>
2019-07-13 21:53:37 +0000

    Update documentation

---
+++ implementors/vst/plugin/trait.Plugin.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vst"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
